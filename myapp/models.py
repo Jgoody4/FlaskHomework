0f136e7f8a9ac08d21ef79fcba3963c886300201
Justin Goodman <justin.goodman@sjsu.edu>
2021-11-09 17:02:39 -0800

    This commit finishes up the code with the database which was initialized in the python3 terminal (for my reference).

--- myapp/models.py
+++ myapp/models.py
@@ -1,20 +1,5 @@
 from myapp import db
 
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(128), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
-
-	def __repr__(self):
-		return f'<User {self.username}>'
-
-	def check_password(self, password):
-		return check_password_hash(self.password_hash, password)
-
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
-
 class Cities(db.Model):
 	city_name = db.Column(db.String(64), index=True)
 	city_rank = db.Column(db.Integer, index=True)
--- myapp/routes.py
+++ myapp/routes.py
@@ -4,27 +4,7 @@ from myapp.forms import TopCities
 from myapp.models import Cities
 from flask import render_template, flash, redirect
 
-@myobj.route("/")
-def home():
-	"""Return H1 header that says welcome!
-	"""
-	return render_template("main.html")
-
-@myobj.route("/hi")
-def hi():
-	return "Hi!"
-
-@myobj.route("/main")
-def main():
-	date = '2021-10-05'
-	users = {'username' : 'carlos'}
-
-	post = [ {'author': 'John', 'body': 'Beatiful day in Portland!'},\
-		{ 'author': 'Susan', 'body': 'The dat is cloudy today!'}]
-
-	return render_template('hello.html', users=users, datee=date, post=post)
-
-@myobj.route("/home")
+@myobj.route("/", methods=['GET', 'POST'])
 def home_top_cities():
 	title = 'Top Cities'
 	name = 'Justin'
@@ -33,9 +13,9 @@ def home_top_cities():
 		flash(f'City name: {form.city_name.data}, with city rank: {form.city_rank.data}')
 		city_name = form.city_name.data
 		city_rank = form.city_rank.data
-		cities = Cities(city_name=city_name, city_rank=city_rank, is_visted = form.is_visted.data)
+		cities = Cities(city_name=city_name, city_rank=city_rank, is_visited = form.is_visited.data)
 		db.session.add(cities)
 		db.session.commit()
-		return redirect('/home')
+		return redirect('/')
 	top_cities = Cities.query.order_by(Cities.city_rank).all()
 	return render_template('home.html', title=title, name=name, form=form, top_cities=top_cities)
